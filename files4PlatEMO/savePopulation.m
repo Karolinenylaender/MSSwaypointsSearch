11a954ca60d69c7a760eb269b7af65fac76de0c3
karolinen <karolinen@simula.no>
2024-09-12 08:59:26 +0200

    add files that needs to be moved to PlatEMO

---
+++ files4PlatEMO/savePopulation.m
@@ -0,0 +1,10 @@
+function savePopulation(obj, Population)
+    global shipResultsPath
+    
+    
+    generationPath = append(shipResultsPath, "-g", string(obj.generation))
+    paths = obj.pathsMap
+    save(generationPath,"Population", "paths")
+    obj.generation = obj.generation + 1
+    obj.pathsMap = containers.Map();
+end
