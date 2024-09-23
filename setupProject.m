89555fb4924c9d4bc10531bbbfc5aab0b44c9b67
karolinen <karolinen@simula.no>
2024-09-23 13:32:55 +0200

    update project setup

--- setupProject.m
+++ setupProject.m
@@ -1,8 +1,30 @@
 
-addpath("evaluationFunctions")
-addpath("pathGeneration")
-addpath("multiobjectiveSearch")
-addpath("pareto_HV")
 
-addpath("BIMK-PlatEMO-4.7.0.0/PlatEMO/")
-addpath("ShipProblems")
+%addFolderToPath("experiments/")
+%addFolderToPath("pathGeneration")
+%addFolderToPath("multiobjectiveSearch")
+%addFolderToPath("pareto_HV")
+
+addFolderToPath("BIMK-PlatEMO-4.7.0.0/PlatEMO/")
+addFolderToPath("MSS")
+addFolderToPath("ShipProblems")
+addFolderToPath("NSGA-II-Adapted/")
+%addFolderToPath(extract)
+addFolderToPath("ExperimentFunctions/")
+
+% Save the updated path
+savepath;
+fprintf('MATLAB path updated and saved successfully in MATLAB.\n');
+
+function addFolderToPath(folderName)
+    folderInfo = what(folderName);
+    if ~isempty(folderInfo)
+        basePath = char(folderInfo.path);  % Ensure the path is char
+    end
+
+    warning('off', 'MATLAB:rmpath:DirNotFound');
+    rmpath(genpath(basePath));
+    warning('on', 'MATLAB:rmpath:DirNotFound');
+
+    addpath(genpath(basePath));
+end
