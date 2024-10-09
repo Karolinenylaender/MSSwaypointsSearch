a7c59de7bb325b6cb60875dcf3956e6f520e6647
karolinen <karolinen@simula.no>
2024-10-09 15:09:18 +0200

    add function to the script

--- ProcessResultsFunctions/featuresExtraction/CalculateSearchPerformancePerSubpath.m
+++ ProcessResultsFunctions/featuresExtraction/CalculateSearchPerformancePerSubpath.m
@@ -1,32 +1,39 @@
 function [experimentPerformance] = CalculateSearchPerformancePerSubpath(ship, searchProcess, experimentNumber, populationSize, numGenerations)
     experimentPerformance = [];
+    numMetrics = 4;
+
+    if ship == "remus100"
+        numInitialWaypoints = 6;
+    elseif ship == "nspauv"
+        numInitialWaypoints = 6;
+    elseif ship == "mariner"
+        numInitialWaypoints = 7;
+    end
+
     for generation = 1:numGenerations
         [Population, paths] = loadResults(ship, searchProcess, experimentNumber, populationSize, generation);
         Dec = Population.decs;
         for individualIndex = 1:size(Dec,1)
-            individual = Dec(individualIndex,:);
+            %individual = Dec(individualIndex,:);
             individualPath = paths(string(individualIndex));
             fullPath = individualPath('fullpath');
             transitionIndices = individualPath('transitionIndices');
             angles = individualPath('angles');
 
-            pointsMatrix  =  reshape(individual, [3, 6])';
-
             individualPerformance = extractFeaturesPath(fullPath, angles, transitionIndices);
             if transitionIndices(1) == length(fullPath)
                 % all subpaths are missing
-                plotShipPath(fullPath,pointsMatrix,5)
-                numMissingPaths = length(pointsMatrix);
+                numMissingPaths = numInitialWaypoints; 
                   
                 individualPerformance = -ones(numMissingPaths, numMetrics);
 
-            elseif length(transitionIndices) < (length(pointsMatrix))
+            elseif length(transitionIndices) < numInitialWaypoints
                 % one or more subpaths are missing
-                numMissingPaths = length(pointsMatrix) - length(transitionIndices);
+                numMissingPaths = numInitialWaypoints - length(transitionIndices);
                 missingPathPerformance = -ones(numMissingPaths, numMetrics);
                 individualPerformance = [individualPerformance; missingPathPerformance];
             end
-            experimentPerformance = [experimentPerformance; individualPerformance]
+            experimentPerformance = [experimentPerformance; individualPerformance];
         end
     end
     
@@ -40,16 +47,32 @@ function fullpathPerformance = extractFeaturesPath(fullPath, angles, transitionI
         startPoint = fullPath(transitionIndices(pathSectionIndex),:);
         endPoint = fullPath(transitionIndices(pathSectionIndex+1),:);
         
-        numPeaksForAngles = zeros(size(currentAngles,1),1);
-        for angleType = 1:size(currentAngles,1)
-            [AnglesNumPeaks, ~] = calculateNumberOfPeaks(currentAngles);
+        numPeaksForAngles = zeros(size(currentAngles,2),1);
+        for angleType = 1:size(currentAngles,2)
+            [AnglesNumPeaks, ~] = calculateNumberOfPeaks(currentAngles(:,angleType));
             numPeaksForAngles(angleType) = AnglesNumPeaks;
         end
         lengthOfPath = length(currentPath)/pdist2(startPoint,endPoint);
 
-        currentPathFeatures = [lengthOfPath, numPeaksForAngles];
-        
-        fullpathPerformance = [fullpathPerformance; currentPathFeatures];
+        currentPathFeatures = [lengthOfPath; numPeaksForAngles];
+
+        fullpathPerformance = [fullpathPerformance; currentPathFeatures'];
     end
 
+end
+
+function [numPeaks, validPeaks] = calculateNumberOfPeaks(measurement)
+    neighborhoodSize = floor(length(measurement)/100);
+    threshold = 0.01;
+
+    [autocorrelation, ~] = xcorr(measurement, 'coeff');
+    [peaks,~] = findpeaks(autocorrelation, 'MinPeakDistance',neighborhoodSize);
+    
+    validPeaks = peaks(peaks > threshold);
+    numPeaks = length(validPeaks) - 1 ;
+    if numPeaks < 0
+        numPeaks = 0;
+    end
+
+   
 end
