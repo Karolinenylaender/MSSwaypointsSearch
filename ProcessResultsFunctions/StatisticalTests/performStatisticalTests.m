fa51b3b9f4d50728b01f93352f8fd91b8f1326e4
karolinen <karolinen@simula.no>
2024-10-25 15:51:45 +0200

    add function to calculate the IGD

--- ProcessResultsFunctions/StatisticalTests/performStatisticalTests.m
+++ ProcessResultsFunctions/StatisticalTests/performStatisticalTests.m
@@ -22,15 +22,39 @@ PopulationRandomSearch = loadAllShipPopulations(ship, "randomSearch",RandomValid
 NSGAObjectives = PopulationNSGASearch.objs;
 randomObjectives = PopulationRandomSearch.objs;
 combinedObjectives = [NSGAObjectives; randomObjectives];
+
 maxObjectives = [max(combinedObjectives(:,1)), max(combinedObjectives(:,2))];
 minObjectives = [min(combinedObjectives(:,1)), min(combinedObjectives(:,2))];
+% 
+% combinedRanges = [];
+% for experimentNumber = SearchValidExperiments
+%     experimentPopulation = loadExperimentsPopulations(ship, "minDistanceMaxPath",experimentNumber, populationSize, numGenerations);
+%     experimentObjectives = experimentPopulation.objs;
+%     ranges = [max(experimentObjectives(:,1)) max(experimentObjectives(:,2)) min(experimentObjectives(:,1)) min(experimentObjectives(:,2))];
+%     combinedRanges = [combinedRanges; ranges];
+% end
+% for experimentNumber = RandomValidExperiments
+%     experimentPopulation = loadExperimentsPopulations(ship, "randomSearch",experimentNumber, populationSize, numGenerations);
+%     experimentObjectives = experimentPopulation.objs;
+%     ranges = [max(experimentObjectives(:,1)) max(experimentObjectives(:,2)) min(experimentObjectives(:,1)) min(experimentObjectives(:,2))];
+%     combinedRanges = [combinedRanges; ranges];
+% end
+% 
+% smallestMaxValues = [min(combinedRanges(:,1)) min(combinedRanges(:,2))];
+% largestMinValues = [max(combinedRanges(:,3)) max(combinedRanges(:,4))];
 
+
+%maxObjectives = smallestMaxValues;
+%minObjectives = largestMinValues;
 
 NSGANormalizedHVScores = calculateSearchHvNormalized(ship, "minDistanceMaxPath",SearchValidExperiments, populationSize, numGenerations,maxObjectives, minObjectives);
 NSGAHVScores =  calculateSearchHV(ship, "minDistanceMaxPath",SearchValidExperiments, populationSize, numGenerations,maxObjectives, minObjectives);
+NSGAIGDscore = calculateSearchIGDScore(ship, "minDistanceMaxPath",SearchValidExperiments, populationSize, numGenerations,maxObjectives, minObjectives, PopulationNSGASearch.best.objs)
 
 RandomNormalizedHVScores = calculateSearchHvNormalized(ship, "randomSearch",RandomValidExperiments, populationSize, numGenerations,maxObjectives, minObjectives);
 RandomHVScores =  calculateSearchHV(ship, "randomSearch",RandomValidExperiments, populationSize, numGenerations,maxObjectives, minObjectives);
+RandomIGDscore = calculateSearchIGDScore(ship, "randomSearch",RandomValidExperiments, populationSize, numGenerations,maxObjectives, minObjectives, PopulationRandomSearch.best.objs)
+
 
 
 figure(1)
@@ -54,6 +78,16 @@ boxplot([NSGAHVScores(:,2), RandomHVScores(:,2)])
 xticklabels({"NSGA search", "Random search"})
 title("Hypervolume: PlatEMO function - not normalized")
 
+figure(5)
+boxplot([NSGAIGDscore(:,1), RandomIGDscore(:,1)])
+xticklabels({"NSGA search", "Random search"})
+title("IGD score - not normalized")
+
+figure(6)
+boxplot([NSGAIGDscore(:,2), RandomIGDscore(:,2)])
+xticklabels({"NSGA search", "Random search"})
+title("IGD score - normalized ")
+
    
 
 
@@ -63,7 +97,7 @@ function HVscores = calculateSearchHV(ship, searchProcess,validExperiments, popu
     for experimentNumber = validExperiments
         experimentPopulation = loadExperimentsPopulations(ship, searchProcess,experimentNumber, populationSize, numGenerations);
 
-        scores = [hypervolume(experimentPopulation.best.objs, referencePoint, length(experimentPopulation.best.objs)) HV(experimentPopulation,referencePoint)];
+        scores = [hypervolume(experimentPopulation.best.objs, referencePoint, 10*length(experimentPopulation.best.objs)) HV(experimentPopulation,referencePoint)];
         HVscores = [HVscores; scores];
     end
 end
@@ -77,11 +111,29 @@ function HVscores = calculateSearchHvNormalized(ship, searchProcess,validExperim
 
         normalizedObjecitves = (experimentObjectives - minValues.*ones(length(experimentObjectives),length(minValues))) ./ (maxValues-minValues);
         experimentPopulation = SOLUTION(experimentPopulation.decs, normalizedObjecitves, experimentPopulation.cons);
-        scores = [hypervolume(experimentPopulation.best.objs, referencePoint, length(experimentPopulation.best.objs)) HV(experimentPopulation,referencePoint)];
+        scores = [hypervolume(experimentPopulation.best.objs, [1 1], 10*length(experimentPopulation.best.objs)) HV(experimentPopulation,[1 1])]; %% TODO 1 1 for HV function might be incorrect
 
         HVscores = [HVscores; scores];
     end
 end
 
 
+
+function IGDscore = calculateSearchIGDScore(ship, searchProcess,validExperiments, populationSize, numGenerations,maxValues, minValues, paretoFront)
+    IGDscore = [];
+    for experimentNumber = validExperiments
+         experimentPopulation = loadExperimentsPopulations(ship, searchProcess,experimentNumber, populationSize, numGenerations);
+         score = IGD(experimentPopulation,paretoFront);
+
+         experimentObjectives = experimentPopulation.objs;
+         normalizedObjectives = (experimentObjectives - minValues.*ones(length(experimentObjectives),length(minValues))) ./ (maxValues-minValues);
+         experimentPopulation = SOLUTION(experimentPopulation.decs, normalizedObjectives, experimentPopulation.cons);
+         paretoFrontNormalized = (paretoFront - minValues.*ones(length(paretoFront),length(minValues))) ./ (maxValues-minValues);
+
+         NormalizedScore = IGD(experimentPopulation, paretoFrontNormalized);
+         IGDscore = [IGDscore; [score, NormalizedScore]];
+
+    end
+    
+end
 
